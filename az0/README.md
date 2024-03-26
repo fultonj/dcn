0bc8ddfe319ac4912211125345cfcca78fed94f9
John Fulton <fulton@redhat.com>
2024-03-26 16:51:59 -0400

    clean up az0 readme

--- az0/README.md
+++ az0/README.md
@@ -12,30 +12,11 @@ the [design example](../design.md).
 - `oc get pods -n openstack-operators` returns a list of running
   OpenStack operators
 
-- `oc wait pod -n metallb-system -l component=speaker --for
-  condition=Ready --timeout=300s` should indicate that all
-  speaker pod conditions have been met.
-
-- `oc wait deployments/nmstate-webhook -n openshift-nmstate --for
-  condition=Available --timeout=300s` should return
-  `deployment.apps/nmstate-webhook condition met`
-
 ## Overview
 
 This directory contains CRs which should be applied and verified in
-the order which is documented in here. You should be able to
-take these example CRs and modify them for your environment. The
-optional section of each stage describes the commands which were used
-to generate the CRs.
-
-For CR generation in this section it is possible to use the
-[HCI VA](https://github.com/openstack-k8s-operators/architecture/tree/main/examples/va/hci)
-with the
-[ci-framework](https://github.com/openstack-k8s-operators/ci-framework).
-For transparency and repeatability in my environment I have documented
-the commands that I used to do this. The VA and how it is kustomized
-is still experimental, and not part of a product, though it can be used
-to solve the problem of environment variable substitution.
+the order which is documented here. Anyone should be able to take
+these example CRs and modify them for their environment.
 
 ## CRs: networking and control plane
 
@@ -54,16 +35,6 @@ oc apply -f control-plane.yaml
 oc wait osctlplane controlplane --for condition=Ready --timeout=600s
 ```
 
-### Optional CR generation
-
-```
-pushd ~/src/github.com/openstack-k8s-operators/architecture/examples/va/hci/
-cp ~/ci-framework-data/artifacts/ci_gen_kustomize_values/network-values/values.yaml control-plane/nncp/values.yaml
-kustomize build control-plane/nncp > nncp.yaml
-kustomize build control-plane > control-plane.yaml
-popd
-```
-
 ## CRs: pre-ceph data plane
 
 - [dataplane-pre-ceph.yaml](dataplane-pre-ceph.yaml)
@@ -75,16 +46,6 @@ oc apply -f dataplane-pre-ceph.yaml
 oc get pods -w -l app=openstackansibleee
 oc wait osdpd edpm-deployment-pre-ceph --for condition=Ready --timeout=1200s
 ```
-
-### Optional CR generation
-
-```
-pushd ~/src/github.com/openstack-k8s-operators/architecture/examples/va/hci/
-cp ~/ci-framework-data/artifacts/ci_gen_kustomize_values/edpm-values/values.yaml edpm-pre-ceph/values.yaml
-kustomize build edpm-pre-ceph > dataplane-pre-ceph.yaml
-popd
-```
-Ensure the `nodes` list only has has three computes.
 
 ## Deploy Ceph for AZ0
 
@@ -98,29 +59,6 @@ do this are documented
 [upstream](https://github.com/openstack-k8s-operators/docs/blob/main/ceph.md)
 and in the Deployment document from
 [Development Preview](https://access.redhat.com/rhosp-18-dev-preview-3-release-notes).
-
-### Optional
-
-For test environments, the ci-framework may be used to deploy Ceph.
-
-```
-export START=100
-cd ~/src/github.com/openstack-k8s-operators/ci-framework/
-export N=2
-echo -e "localhost ansible_connection=local\n[computes]" > inventory.yml
-for I in $(seq $START $((N+100))); do
-  echo 192.168.122.${I} >> inventory.yml
-done
-
-ln -s ~/dcn/lib/ceph_az0.yaml
-ln -s ~/hci.yaml
-
-export ANSIBLE_REMOTE_USER=zuul
-export ANSIBLE_SSH_PRIVATE_KEY=~/.ssh/id_cifw
-export ANSIBLE_HOST_KEY_CHECKING=False
-
-ANSIBLE_GATHERING=implicit ansible-playbook playbooks/ceph.yml -e @hci.yaml -e @ceph_az0.yaml
-```
 
 ## CRs: post-ceph control and data plane
 
@@ -139,17 +77,7 @@ Wait for post-Ceph data plane deployment to finish
 oc wait osdpd edpm-deployment-post-ceph --for condition=Ready --timeout=1200s
 ```
 
-### Optional CR generation
-```
-pushd ~/src/github.com/openstack-k8s-operators/architecture/examples/va/hci/
-cp /tmp/edpm_values_post_ceph.yaml ~/src/github.com/openstack-k8s-operators/architecture/examples/va/hci/values.yaml
-cp /tmp/edpm_service_values_post_ceph.yaml ~/src/github.com/openstack-k8s-operators/architecture/examples/va/hci/service-values.yaml
-kustomize build > post-ceph.yaml
-popd
-```
-Ensure the `nodes` list only has has three computes.
-
-## Finialize Nova computes
+## Discover Nova computes in AZ0
 
 Ask Nova to discover all compute hosts in AZ0.
 ```
---
+++ extra/az0.sh
@@ -0,0 +1,191 @@
+#!/bin/bash
+
+# Personal script for automting AZ0 deployment using:
+# - https://github.com/openstack-k8s-operators/ci-framework
+# - https://github.com/openstack-k8s-operators/architecture/tree/main/examples/va/hci
+# 
+# The above and how it is kustomized is still experimental, and not part
+# of the product, though it can be used to solve the problem of environment
+# creation and environment variable substitution for testing.
+
+OPERATORS=0
+METALLB=0
+NMSTATE=0
+
+CONTROLPLANE=0
+DATAPLANE=0
+CEPH=0
+POSTCEPH=0
+DISCOVER=0
+
+export PASS=$(cat ~/.kube/kubeadmin-password)
+oc login -u kubeadmin -p $PASS https://api.ocp.openstack.lab:6443
+if [[ $? -gt 0 ]]; then
+    exit 1
+fi
+
+pushd ~/src/github.com/openstack-k8s-operators/architecture
+
+if [ $OPERATORS -eq 1 ]; then
+    echo "oc get pods -w -n openstack-operators"
+    # kustomize build examples/common/olm/
+
+    oc apply -k examples/common/olm/
+
+    while ! (oc get pod --no-headers=true -l name=cert-manager-operator -n cert-manager-operator| grep "cert-manager-operator"); do sleep 10; done
+    
+    oc wait pod -n cert-manager-operator --for condition=Ready -l name=cert-manager-operator --timeout=300s
+
+    while ! (oc get pod --no-headers=true -l app=cainjector -n cert-manager | grep "cert-manager-cainjector"); do sleep 10; done
+    oc wait pod -n cert-manager -l app=cainjector --for condition=Ready --timeout=300s
+
+    while ! (oc get pod --no-headers=true -l app=webhook -n cert-manager | grep "cert-manager-webhook"); do sleep 10; done
+    oc wait pod -n cert-manager -l app=webhook --for condition=Ready --timeout=300s
+
+    while ! (oc get pod --no-headers=true -l app=cert-manager -n cert-manager | grep "cert-manager"); do sleep 10; done
+    oc wait pod -n cert-manager -l app=cert-manager --for condition=Ready --timeout=300s
+
+    timeout 300 bash -c "while ! (oc get pod --no-headers=true -l control-plane=controller-manager -n metallb-system | grep metallb-operator-controller); do sleep 10; done"
+    
+    oc wait pod -n metallb-system --for condition=Ready -l control-plane=controller-manager --timeout=300s
+
+    timeout 300 bash -c "while ! (oc get pod --no-headers=true -l component=webhook-server -n metallb-system | grep metallb-operator-webhook); do sleep 10; done"
+
+    oc wait pod -n metallb-system --for condition=Ready -l component=webhook-server --timeout=300s
+
+    timeout 300 bash -c "while ! (oc get deployments/nmstate-operator -n openshift-nmstate); do sleep 10; done"
+    oc wait deployments/nmstate-operator -n openshift-nmstate --for condition=Available --timeout=300s
+    
+fi
+
+if [ $METALLB -eq 1 ]; then
+    oc apply -k examples/common/metallb/
+    timeout 300 bash -c "while ! (oc get pod --no-headers=true -l component=speaker -n metallb-system | grep speaker); do sleep 10; done"
+    oc wait pod -n metallb-system -l component=speaker --for condition=Ready --timeout=300s
+fi
+
+if [ $NMSTATE -eq 1 ]; then
+    oc apply -k examples/common/nmstate/
+
+    timeout 300 bash -c "while ! (oc get pod --no-headers=true -l component=kubernetes-nmstate-handler -n openshift-nmstate| grep nmstate-handler); do sleep 10; done"
+    oc wait pod -n openshift-nmstate -l component=kubernetes-nmstate-handler --for condition=Ready --timeout=300s
+    timeout 300 bash -c "while ! (oc get deployments/nmstate-webhook -n openshift-nmstate); do sleep 10; done"
+    oc wait deployments/nmstate-webhook -n openshift-nmstate --for condition=Available --timeout=300s
+
+fi
+
+oc project openstack > /dev/null
+
+if [ $CONTROLPLANE -eq 1 ]; then    
+    SRC=~/ci-framework-data/artifacts/ci_gen_kustomize_values/network-values/values.yaml
+    if [[ ! -e $SRC ]]; then
+        echo "$SRC is missing"
+        exit 1
+    fi
+    echo -e "\noc get pods -n openstack\n"
+    
+    pushd examples/va/hci/
+    cp $SRC control-plane/nncp/values.yaml
+    kustomize build control-plane/nncp > nncp.yaml
+    kustomize build control-plane > control-plane.yaml
+
+    oc apply -f nncp.yaml
+    oc wait nncp -l osp/nncm-config-type=standard --for jsonpath='{.status.conditions[0].reason}'=SuccessfullyConfigured --timeout=300s
+
+    sleep 5
+    oc apply -f control-plane.yaml
+    sleep 5
+    oc wait osctlplane controlplane --for condition=Ready --timeout=600s
+
+    popd
+fi
+
+if [ $DATAPLANE -eq 1 ]; then
+    SRC=~/ci-framework-data/artifacts/ci_gen_kustomize_values/edpm-values/values.yaml
+    if [[ ! -e $SRC ]]; then
+        echo "$SRC is missing"
+        exit 1
+    fi
+    echo -e "\noc get pods -w -l app=openstackansibleee\n"
+
+    pushd examples/va/hci/
+    cp $SRC edpm-pre-ceph/values.yaml
+    
+    NODES=$(grep edpm-compute edpm-pre-ceph/values.yaml | awk {'print $1'} | sort | uniq | wc -l)
+    if [[ ! $NODES -eq 3 ]]; then
+        echo "Aborting. You only want to deploy 3 nodes, not $NODES"
+        grep edpm-compute $SRC | awk {'print $1'} | sort | uniq
+        echo $SRC
+        exit 1
+    fi
+    kustomize build edpm-pre-ceph > dataplane-pre-ceph.yaml
+    oc create -f dataplane-pre-ceph.yaml
+    oc wait osdpd edpm-deployment-pre-ceph --for condition=Ready --timeout=1200s
+
+    popd
+fi
+
+if [ $CEPH -eq 1 ]; then
+    export START=100
+    pushd ~/src/github.com/openstack-k8s-operators/ci-framework/
+    export N=2
+    echo -e "localhost ansible_connection=local\n[computes]" > inventory.yml
+    for I in $(seq $START $((N+100))); do
+        echo 192.168.122.${I} >> inventory.yml
+    done
+    export ANSIBLE_REMOTE_USER=zuul
+    export ANSIBLE_SSH_PRIVATE_KEY=~/.ssh/id_cifw
+    export ANSIBLE_HOST_KEY_CHECKING=False
+
+    ansible -i inventory.yml -m ping computes
+    if [ $? -gt 0 ]; then
+        echo "inventory problem"
+        exit 1
+    fi
+    ln -fs ~/dcn/extra/ceph_az0.yaml
+    ln -fs ~/hci.yaml
+    ANSIBLE_GATHERING=implicit ansible-playbook playbooks/ceph.yml -e @hci.yaml -e @ceph_az0.yaml
+    popd
+fi
+
+if [ $POSTCEPH -eq 1 ]; then
+    SRC1=/tmp/edpm_values_post_ceph.yaml
+    SRC2=/tmp/edpm_service_values_post_ceph.yaml
+    for SRC in $SRC1 $SRC2; do
+        if [[ ! -e $SRC ]]; then
+            echo "$SRC is missing"
+            exit 1
+        fi
+    done
+    
+    pushd examples/va/hci/
+    cp $SRC1 ~/src/github.com/openstack-k8s-operators/architecture/examples/va/hci/values.yaml
+    cp $SRC2 ~/src/github.com/openstack-k8s-operators/architecture/examples/va/hci/service-values.yaml
+    kustomize build > post-ceph.yaml
+
+    NODES=$(grep edpm-compute post-ceph.yaml | awk {'print $1'} | sort | uniq | wc -l)
+    if [[ ! $NODES -eq 3 ]]; then
+        echo "Aborting. You only want to deploy 3 nodes, not $NODES"
+        grep edpm-compute post-ceph.yaml | awk {'print $1'} | sort | uniq
+        echo "$PWD/post-ceph.yaml"
+        exit 1
+    fi
+
+    oc apply -f post-ceph.yaml
+
+    echo -e "\noc get pods -n openstack -w\n"
+    echo -e "\noc get pods -w -l app=openstackansibleee\n"
+
+    oc wait osctlplane controlplane --for condition=Ready --timeout=600s
+    oc wait osdpd edpm-deployment-post-ceph --for condition=Ready --timeout=1200s
+
+    popd
+fi
+
+if [ $DISCOVER -eq 1 ]; then
+    oc rsh nova-cell0-conductor-0 nova-manage cell_v2 discover_hosts --verbose
+    oc rsh openstackclient openstack compute service list
+    oc rsh openstackclient openstack network agent list
+fi
+
+popd
