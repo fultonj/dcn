a0cac4565eaac3e31ab21ed418531f6a72f841c0
John Fulton <fulton@redhat.com>
2024-03-27 17:49:07 -0400

    start work on az1

--- az1/README.md
+++ az1/README.md
@@ -2,3 +2,29 @@
 
 In this step we deploy the AZ1 subset of
 the [design example](../design.md).
+
+## CRs: pre-ceph data plane
+
+- [dataplane-pre-ceph-az1.yaml](dataplane-pre-ceph-az1.yaml)
+
+### Apply and Verify
+
+```
+oc apply -f dataplane-pre-ceph-az1.yaml
+oc get pods -w -l app=openstackansibleee
+oc wait osdpd edpm-deployment-pre-ceph-az1 --for condition=Ready --timeout=1200s
+```
+
+## Deploy Ceph for AZ1
+
+In this step the `cephadm` tool should be used to deploy Ceph on
+edpm-compute-3, edpm-compute-4 and edpm-compute-5.
+
+After the Ceph cluster is deployed create pools for Nova, Cinder and
+Glance, and then create an openstack keyring to access them. Add the
+keyring and conf file to the existing Ceph Secret as described in
+[multiple Ceph keyrings and confs in one secret](https://github.com/openstack-k8s-operators/docs/blob/main/ceph.md#regarding-multiple-ceph-keyrings)
+so that a single
+[extraMounts](https://github.com/openstack-k8s-operators/docs/blob/main/ceph.md#access-the-ceph-secret-via-extramounts)
+can be used to popluate `/etc/ceph` for all control plane pods
+so they can access multiple Ceph clusters.
---
+++ extra/az1.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# Personal script for automting AZ1 deployment using:
+# - https://github.com/openstack-k8s-operators/ci-framework
+# - https://github.com/openstack-k8s-operators/architecture/tree/main/examples/va/hci
+# 
+# The above and how it is kustomized is still experimental, and not part
+# of the product, though it can be used to solve the problem of environment
+# creation and environment variable substitution for testing.
+
+DATAPLANE=0
+CEPH=0
+POSTCEPH=0
+DISCOVER=0
+
+export PASS=$(cat ~/.kube/kubeadmin-password)
+oc login -u kubeadmin -p $PASS https://api.ocp.openstack.lab:6443
+if [[ $? -gt 0 ]]; then
+    exit 1
+fi
+
+pushd ~/src/github.com/openstack-k8s-operators/architecture
+
+if [ $DATAPLANE -eq 1 ]; then
+    SRC=~/ci-framework-data/artifacts/ci_gen_kustomize_values/edpm-values/values.yaml
+    if [[ ! -e $SRC ]]; then
+        echo "$SRC is missing"
+        exit 1
+    fi
+    echo -e "\noc get pods -w -l app=openstackansibleee\n"
+
+    pushd examples/va/hci/
+    python ~/dcn/extra/node_filter.py $SRC edpm-pre-ceph/values.yaml --beg 3 --end 5
+    kustomize build edpm-pre-ceph > dataplane-pre-ceph-az1.yaml
+
+    # need to do additional edits to dataplane-pre-ceph-az1.yaml to make another nodeset
+    
+    
+    # oc create -f dataplane-pre-ceph.yaml
+    # oc wait osdpd edpm-deployment-pre-ceph --for condition=Ready --timeout=1200s
+    popd
+fi
+
+if [ $CEPH -eq 1 ]; then
+    bash ~/dcn/extra/ceph.sh 103 ceph_az1.yaml
+fi
+
+popd
